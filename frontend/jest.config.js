cb23a75eeba8e52937e62deda0b9cb338d5cd3dc
francescaboe <francesca.boe.ac@gmail.com>
2024-08-20 19:32:26 +0200

    fe: add aliases to jest.config

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,4 +2,12 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^components/(.*)$': '<rootDir>/src/components/$1',
+    '^utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^types/(.*)$': '<rootDir>/src/types/$1',
+    '^constants/(.*)$': '<rootDir>/src/constants/$1',
+    '^api/(.*)$': '<rootDir>/src/api/$1',
+    '^routes/(.*)$': '<rootDir>/src/routes/$1',
+  },
 };
